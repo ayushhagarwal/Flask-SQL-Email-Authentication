ffa9538edca4c819eda048a216d45486191a5846
ayushhagarwal <ayushgrwl365@gmail.com>
2021-04-18 17:13:15 +0530

    basic page setup of signup and login complete

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app=Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__=="__main__":
     app.run(host ='0.0.0.0', port = 5001, debug = True)     
